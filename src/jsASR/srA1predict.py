e1584a6bf60ca209cb86d8d9709c998a62ba21b8
Marcus Ng <mn03@mrc-cbu.cam.ac.uk>
2025-08-12 17:59:58 +0100

    added different file out names

--- src/jsASR/srA1predict.py
+++ src/jsASR/srA1predict.py
@@ -13,15 +13,17 @@ Model = tf.keras.Model
 
 from .DataGenTimit import DataGenerator
 
-def predictPhonemeProbabilitiesCausalNN( filename_X: str, filename_Y: str, model_name = 'srA1_k_1', data_split_factor = 0.9):
+def predictPhonemeProbabilitiesCausalNN(
+    filename_X: str, filename_Y: str, model_name: str, 
+    file_identifier_out: str, data_split_factor = 0.9 
+):
     ''' data_split_factor = 0 to predict and store for all timesteps. set to 0.9 for evaluation of development set (and use X of train+dev set)
         see also srA1eval script for evaluation purposes '''
     X = np.load( filename_X )
     Y = np.load( filename_Y )
     
-    # note that where Y == -1 no phoneme label is given "silence"
     # predict all including first after phoneme boundary
-    idx = np.flatnonzero(Y > -1)
+    idx = np.arange(len(Y))
     
     num_timesteps = 50
     batch_size    = 1024
@@ -32,36 +34,50 @@ def predictPhonemeProbabilitiesCausalNN( filename_X: str, filename_Y: str, model
     idx_val   = idx[data_split:]
     
     print('evaluating and predicting... this will take a while')
-    model = cast(Model, Model.load_model( model_name + '.keras'))
+    model = cast(Model, tf.keras.models.load_model( model_name + '.keras'))
 
-    predict_generator = DataGenerator(idx_val, X, Y, dim, reduce_factor = 1, shuffle = False)
+    predict_generator = DataGenerator(
+        idx = idx_val, X=X, Y=Y, out_dim = dim, 
+        reduce_factor = 1, shuffle = False
+    )
     evaluation = model.evaluate(predict_generator, verbose="1", return_dict=True)
     p = model.predict(predict_generator, verbose="1")
 
     idx_valed = np.array(idx_val)[np.array(range(len(p)))] + num_timesteps
-
+    
     # safe log
     eps = 1e-12
     logp = np.log(np.clip(p, eps, 1.0))
-    np.save( model_name + '_logp.npy', logp)
+    np.save( file_identifier_out + '_logp.npy', logp)
     
     y_pred = np.argmax(p,axis=1)
     y_true = Y[idx_valed]
     pred_correct = sum( ( np.logical_or( y_pred[:-8] == y_true[:-8], y_pred[8:] == y_true[:-8]  ) ) ) / (len(y_pred)-8)
-    np.save( model_name + '_Phonemes39_pred.npy', y_pred)
-    np.save( model_name + '_Phonemes39_true.npy',y_true)
+    np.save( file_identifier_out + '_Phonemes39_pred.npy', y_pred)
+    np.save( file_identifier_out + '_Phonemes39_true.npy',y_true)
     
     print(evaluation)   
     print(pred_correct)
     
-def combineModelCausalNN( filename_X1: str, filename_X2: str, filename_Y: str, model_name_1 = 'srA1_k_1', model_name_2 = 'srA1_h_0', model_name_out = 'srA1'):
+def combineModelCausalNN(
+    filename_X1: str, filename_X2: str, filename_Y: str, 
+    model_name_1 = 'srA1_k_1', model_name_2 = 'srA1_h_0', 
+    file_identifier_out = 'srA1',
+):
     ''' combine models based on level and cepstral coefficients (or any two models) by adding their log probabilities '''
 
-    predictPhonemeProbabilitiesCausalNN( filename_X = filename_X1, filename_Y = filename_Y, model_name = model_name_1, data_split_factor = 0)
-    logp_1  = np.load( model_name_1 + '_logp.npy')
-    predictPhonemeProbabilitiesCausalNN( filename_X = filename_X2, filename_Y = filename_Y, model_name = model_name_2, data_split_factor = 0)
-    logp_2  = np.load( model_name_2 + '_logp.npy')
+    predictPhonemeProbabilitiesCausalNN(
+        filename_X = filename_X1, filename_Y = filename_Y, model_name = model_name_1, 
+        file_identifier_out=f'{file_identifier_out}_1', data_split_factor = 0
+    )
+    logp_1  = np.load( f'{file_identifier_out}_1' + '_logp.npy')
+    
+    predictPhonemeProbabilitiesCausalNN(
+        filename_X = filename_X2, filename_Y = filename_Y, model_name = model_name_2,
+        file_identifier_out=f'{file_identifier_out}_2', data_split_factor = 0
+    )
+    logp_2  = np.load( f'{file_identifier_out}_2' + '_logp.npy')
     
     logp = logp_1 + logp_2
-    np.save( model_name_out + '_logp_combined_all.npy', logp)
-    np.save( model_name_out + '_p_combined_all.npy', np.exp(logp))
+    np.save( file_identifier_out + '_logp_combined_all.npy', logp)
+    np.save( file_identifier_out + '_p_combined_all.npy', np.exp(logp))
--- src/jsASR/srA2predict.py
+++ src/jsASR/srA2predict.py
@@ -9,11 +9,18 @@ from typing import cast
 import numpy as np
 
 import tensorflow as tf
-Model = tf.keras.Model
+from keras import Model
 
+from sklearn.utils import compute_class_weight
 from .DataGenTimitTri import DataGeneratorTri
 
-def predictPhonemeProbabilitiesNonCausalNN( filename_X: str, filename_Y: str, model_name = 'srA2_d_14', data_split_factor = 0.9):
+def predictPhonemeProbabilitiesNonCausalNN( 
+    filename_X: str,
+    filename_Y: str, 
+    model_name: str, 
+    file_identifier_out: str,
+    data_split_factor = 0.9 
+):
     ''' use data_split_factor = 0 for running the model on all data or evaluating a test set, and 0.9for evaluating a train+dev set '''  
 
     X = np.load(filename_X)
@@ -22,6 +29,13 @@ def predictPhonemeProbabilitiesNonCausalNN( filename_X: str, filename_Y: str, mo
 
     # note that where Y == -1 no phoneme label is given "silence"
     # predict all including first after phoneme boundary
+    classes = np.array([int(c) for c in np.unique(Y)])
+    weights = compute_class_weight(
+        class_weight = 'balanced',
+        classes = classes,
+        y=Y
+    )
+    cw_dict = dict(zip(classes, weights))
     idx = np.flatnonzero(Y > -1)
     
     batch_size   = 1024
@@ -33,26 +47,29 @@ def predictPhonemeProbabilitiesNonCausalNN( filename_X: str, filename_Y: str, mo
     idx_val   = idx[data_split:]
     
     print('evaluating and predicting... this will take a while')
-    model = cast(Model, tf.keras.models.load_model( model_name + '.keras'))
+    model = cast(Model, tf.keras.models.load_model( model_name + '.keras' ))
     
-    predict_generator = DataGeneratorTri(idx_val, X, Y, dim, reduce_factor = 1,non_causal_steps = int(num_timesteps/2), shuffle = False)
+    predict_generator = DataGeneratorTri(
+        idx=idx_val, X=X, Y=Y, out_dim=dim, cw_dict=cw_dict, 
+        reduce_factor = 1,non_causal_steps = int(num_timesteps/2), shuffle = False
+    )
     evaluation = model.evaluate(predict_generator, verbose="1", return_dict=True)
     p = model.predict(predict_generator, verbose="1")
     
     p_prev = p[0]
-    p_now = p[1]
+    p_now  = p[1]
     p_next = p[2]
-    np.save( model_name + '_p_prev.npy',p_prev)
-    np.save( model_name + '_p_now.npy',p_now)
-    np.save( model_name + '_p_next.npy',p_next)
+    np.save( file_identifier_out + '_p_prev.npy',p_prev )
+    np.save( file_identifier_out + '_p_now.npy', p_now  )
+    np.save( file_identifier_out + '_p_next.npy',p_next )
     
     idx_valed = np.array(idx_val)[np.array(range(len(p_now)))] + num_timesteps - int(num_timesteps/2)
     
     y_pred = np.argmax(p_now,axis=1)
     y_true = Y[idx_valed]
     pred_correct = sum( ( np.logical_or( y_pred[:-8] == y_true[:-8], y_pred[8:] == y_true[:-8]  ) ) ) / (len(y_pred)-8)
-    np.save( model_name + '_Phonemes39_pred.npy', y_pred)
-    np.save( model_name + '_Phonemes39_true.npy',y_true)
+    np.save( file_identifier_out + '_Phonemes39_pred.npy', y_pred)
+    np.save( file_identifier_out + '_Phonemes39_true.npy', y_true)
     
     print(evaluation)
     print(pred_correct)
